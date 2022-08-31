97fad1accb87becb9ed6f7952f6dbbed3d74e373
Nick <59822256+Archasion@users.noreply.github.com>
2022-08-31 22:50:02 +0100

    Update getAuthResponse.js

--- src/getAuthResponse.js
+++ src/getAuthResponse.js
@@ -1,5 +1,5 @@
 const CLIENT_ID = "710407168200802384";
-const REDIRECT_URI = "https://localhost:8080/";
+const REDIRECT_URI = "http://archasion.github.io/discord-oauth2/";
 
 const AUTH_URL = `https://discord.com/api/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${encodeURI(REDIRECT_URI)}&response_type=token&scope=identify`;
 
